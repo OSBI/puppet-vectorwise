a7755048f5f5b935f45d04b8218183de7a96ab4f
Tom Barber <tom@analytical-labs.com>
2012-04-25 15:10:37 +0100

    read password

--- manifests/control.pp
+++ manifests/control.pp
@@ -33,10 +33,10 @@ class vectorwise::control {
 	}
 	
 	exec { "sort out passwords":
-    command => "mkvalidpw ; touch /etc/passwordset",
+    command => "/home/ingres/ingres/bin/mkvalidpw ; touch /etc/passwordset",
     creates => "/etc/passwordset",
     require => [File["/usr/bin/ingbuildscript.sh"], File["/home/ingres/ingrsp.rsp"], Common::Downloadfile["ingresvw-2.0.2-121-NPTL-com-linux-ingbuild-x86_64.tgz"]],
-    user => "ingres",
+    user => "root",
     path    => "/usr/bin/:/bin/:/home/ingres/ingres/bin:/home/ingres/ingres/utility",
     environment => "II_SYSTEM=/home/ingres",
     
